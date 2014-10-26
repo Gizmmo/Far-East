279e7c9eec8f953fb23b25900340c697d7c9d3ff
Gizmmo <travisscott301@gmail.com>
2014-10-25 21:29:41 -0500

    Added Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey ("left")) {
+			float translation = Time.deltaTime * 10;
+			transform.Translate (-translation, 0, 0);
+		}
+
+		if(Input.GetKey ("right")) {
+			float translation = Time.deltaTime * 10;
+			transform.Translate (translation, 0, 0);
+		}
+
+		if(Input.GetKey ("up")) {
+			float translation = Time.deltaTime * 10;
+			transform.Translate (0, translation, 0);
+		}
+
+		if(Input.GetKey ("down")) {
+			float translation = Time.deltaTime * 10;
+			transform.Translate (0, -translation, 0);
+		}
+	}
+}

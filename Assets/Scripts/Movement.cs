7eed08b0932111f87629ac27ebb20164d10ac7b3
Gizmmo <travisscott301@gmail.com>
2014-10-25 21:46:22 -0500

    Refactored Movement script to use getAxis

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,34 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-	public float speed;
+		public float speed;
 
-	// Use this for initialization
-	void Start () {
-	}
+		// Use this for initialization
+		void Start ()
+		{
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey ("left")) {
-			float translation = Time.deltaTime * speed;
-			transform.Translate (-translation, 0, 0);
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetAxis ("Vertical") != 0) {
+						float translation = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+						transform.Translate (0, translation, 0);
+				} else if (Input.GetAxis ("Horizontal") != 0) {
+						float translation = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+						transform.Translate (translation, 0, 0);
+				}
 		}
-
-		if(Input.GetKey ("right")) {
-			float translation = Time.deltaTime * speed;
-			transform.Translate (translation, 0, 0);
-		}
-
-		if(Input.GetKey ("up")) {
-			float translation = Time.deltaTime * speed;
-			transform.Translate (0, translation, 0);
-		}
-
-		if(Input.GetKey ("down")) {
-			float translation = Time.deltaTime * speed;
-			transform.Translate (0, -translation, 0);
-		}
-	}
 }
